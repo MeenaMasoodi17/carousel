efe08741013588450dd902041e24ac2da3f255a9
Meena-Masoodi <masoodi.meena@gmail.com>
2017-09-13 22:10:40 +0530

    transition

--- carousel.js
+++ carousel.js
@@ -27,12 +27,14 @@ function carouselNext(carousel) {
 	carousel.currentItem++;
 	// If we increased it too much
 	if(carousel.currentItem >= carousel.items.length) {
-        carousel.slide.style.transition = 'none';
+		carousel.slide.style.transition = 'none';
+		console.log(carousel.slide.style);
 		// set it back to the first item
-        carousel.currentItem = 0;
-        // carousel.slide.style.transition = 'margin 0.5s ease-in-out';
-        
+		carousel.currentItem = 0;		
+	} else {
+		carousel.slide.style.transition = 'margin 0.5s ease-in-out';
 	}
+	// TODO: fix animation to first slide from last and vice-versa
 
 	// set style for the current item
 	displayCurrentItem(carousel);
@@ -43,9 +45,12 @@ function carouselPrev(carousel) {
 	carousel.currentItem--;
 	// If we're below zero, we've gone too far
 	if(carousel.currentItem <= -1) {
-        carousel.slide.style.transition = 'none';
+		carousel.slide.style.transition = 'none';
+		console.log(carousel.slide.style);
 		// Set it to the last item
 		carousel.currentItem = carousel.items.length - 1;
+	} else {
+		carousel.slide.style.transition = 'margin 0.5s ease-in-out';
 	}
 
 	// set style for the current item
@@ -57,7 +62,6 @@ function displayCurrentItem(carousel) {
 	startCarouselTimeout(carousel);
 	// set margin-left CSS property
 	carousel.slide.style.marginLeft = -1 * (carousel.currentItem * 304);
-    carousel.slide.style.transition = 'margin 0.5s ease-in-out';
 }
 
 function startCarouselTimeout(carousel) {
